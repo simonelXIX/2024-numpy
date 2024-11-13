049f030073457f0b835a8da602fb5eba7b1d0558
Your Name <you@example.com>
2024-11-13 17:31:14 +0100

    add basic usage

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+a1 = np.array([1,2,3])
+print ("a1",a1)
+
+a2 = np.linspace(0, 100, 26)
+print ("a2",a2)
+
+a3 = np.zeros(100)
+print ("a3",a3)
+
+a4 = np.ones (11)
+print ("a4",a4)
+np.random.seed(1)
+r1=np.random.rand(2,3)
+print("r1",r1)
